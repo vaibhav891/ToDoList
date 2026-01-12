af7bff8141d5b266c031e4fa132df015a8dce414
Vaibhav Sultania <vaibhav.sultania@gmail.com>
2026-01-12 17:49:12 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Vaibhav Sultania on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
