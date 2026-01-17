b926c830bf803a11716c907e49649726089b0644
Vaibhav Sultania <vaibhav.sultania@gmail.com>
2026-01-17 19:09:48 +0530

    First commit

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Vaibhav Sultania on 12/01/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
